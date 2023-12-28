2be390bd8029d6be68fe41cb7565cf3001baf251
Juan Contreras <juan.contreras.g123@gmail.com>
2023-12-27 21:27:04 -0300

    Listo pagina Js

--- assets/js.js
+++ assets/js.js
@@ -1,34 +1,48 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    $(document).ready(function() {
-        $("#calcularTotal").click(function() {
-            let cantidadClavos = parseInt($("#clavos").val());
-            let cantidadPernos = parseInt($("#pernos").val());
-            let cantidadTuercas = parseInt($("#tuercas").val());
-            let nombre = $("#nombre").val();
-            let edad = $("#edad").val() || "[Edad]";
-            // Actualizar el estado de pago
-            $("#estadoPago .clavosCantidad").text(cantidadClavos);
-            $("#estadoPago .clavosTotal").text('$' + (cantidadClavos * 50));
-            $("#estadoPago .pernosCantidad").text(cantidadPernos);
-            $("#estadoPago .pernosTotal").text('$' + (cantidadPernos * 150));
-            $("#estadoPago .tuercasCantidad").text(cantidadTuercas);
-            $("#estadoPago .tuercasTotal").text('$' + (cantidadTuercas * 150));
 
-            // Calcular el total general y actualizar en el estado de pago
-            let totalGeneral = cantidadClavos * 50 + cantidadPernos * 150 + cantidadTuercas * 150;
-            $("#estadoPago .totalGeneral").text('$' + totalGeneral);
+$(document).ready(function() {
+    $("#calcularTotal").click(function() {
+        let cantidadClavos = parseInt($("#clavos").val()) || 0;
+        let cantidadPernos = parseInt($("#pernos").val()) || 0;
+        let cantidadTuercas = parseInt($("#tuercas").val()) || 0;
+        let nombre = $("#nombre").val() || "";
+    let edad = $("#edad").val() || "";
 
-            // Actualizar el stock en productos y detalles
-            // Actualiza el valor de stock para cada producto
-            let stockClavos = 200 - cantidadClavos;
-            let stockPernos = 150 - cantidadPernos;
-            let stockTuercas = 150 - cantidadTuercas;
+        // Actualizar el estado de pago
+        $(".clavosCantidad").text(cantidadClavos);
+        $(".clavosTotal").text('$' + (cantidadClavos * 50));
+        $(".pernosCantidad").text(cantidadPernos);
+        $(".pernosTotal").text('$' + (cantidadPernos * 150));
+        $(".tuercasCantidad").text(cantidadTuercas);
+        $(".tuercasTotal").text('$' + (cantidadTuercas * 150));
 
-            // Actualizar los valores de stock en la tabla de productos y detalles
-            $("#productosDetalles .stockClavos").text(stockClavos);
-            $("#productosDetalles .stockPernos").text(stockPernos);
-            $("#productosDetalles .stockTuercas").text(stockTuercas);
-            
-        });
+        // Calcular el total general y actualizar en el estado de pago
+        let totalGeneral = cantidadClavos * 50 + cantidadPernos * 150 + cantidadTuercas * 150;
+        let totalCantidad = cantidadClavos + cantidadPernos  + cantidadTuercas;
+        $(".totalGeneral").text('$' + totalGeneral);
+        $(".totalCantidad").text(totalCantidad);
+
+
+        // Actualizar el stock en productos y detalles
+        let stockClavos = 200 - cantidadClavos;
+        let stockPernos = 150 - cantidadPernos;
+        let stockTuercas = 150 - cantidadTuercas;
+
+        // Actualizar los valores de stock en la tabla de productos y detalles
+        $(".stockClavos").text(stockClavos);
+        $(".stockPernos").text(stockPernos);
+        $(".stockTuercas").text(stockTuercas);
+        $("#nombreEstadoPago").text(nombre);
+    $("#edadEstadoPago").text(edad);
     });
+
+    $("#nombre").on("input", function() {
+    let nombre = $(this).val();
+    if (/\d/.test(nombre)) {
+        alert("Por favor, ingresa solo letras en el campo de nombre.");
+        $(this).val("");
+    }
+});
+
+});
+
